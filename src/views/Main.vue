9f135deb4b5520e7b4956290b0ca19a7c720817b
Mikko Rajakorpi <mikko.rajakorpi@tuni.fi>
2022-11-24 14:01:17 +0200

    removed loading functionality (didn't work)

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,56 +1,41 @@
 <template>
-  <div v-if="loading">
-    <h1>Loading...</h1>
-  </div>
+  <header>
+    <button @click="this.$router.push('/liked')" class="linkButton">
+      Liked cats
+    </button>
+    <button @click="this.$router.push('/disliked')" class="linkButton">
+      Disliked cats
+    </button>
+    <h1>Rate the cat</h1>
+  </header>
 
-  <div v-else>
-    <header>
-      <button @click="this.$router.push('/liked')" class="linkButton">
-        Liked cats
-      </button>
-      <button @click="this.$router.push('/disliked')" class="linkButton">
-        Disliked cats
-      </button>
-      <h1>Rate the cat</h1>
-    </header>
+  <article>
+    <CatPicture :cat="currentCat" :type="'normal'" />
+    <br />
 
-    <article>
-      <CatPicture :cat="currentCat" :type="'normal'" />
-      <br />
-
-      <button @click="dislikeCat" class="iconButton">
-        <img src="../assets/dislike.svg" alt="Dislike" />
-      </button>
-      <button @click="likeCat" class="iconButton">
-        <img src="../assets/like.svg" alt="Like" />
-      </button>
-    </article>
-  </div>
+    <button @click="dislikeCat" class="iconButton">
+      <img src="../assets/dislike.svg" alt="Dislike" />
+    </button>
+    <button @click="likeCat" class="iconButton">
+      <img src="../assets/like.svg" alt="Like" />
+    </button>
+  </article>
 </template>
 
 <script lang="ts">
 import CatPicture from "../components/CatPicture.vue";
 
 export default {
-  data() {
-    return {
-      loading: true,
-    };
-  },
   methods: {
     likeCat() {
       this.$store.commit("addLiked", this.currentCat);
 
-      this.loading = true;
       this.$store.commit("nextCat");
-      this.loading = false;
     },
     dislikeCat() {
       this.$store.commit("addDisliked", this.currentCat);
 
-      this.loading = true;
       this.$store.commit("nextCat");
-      this.loading = false;
     },
   },
   computed: {
@@ -64,9 +49,7 @@ export default {
   mounted() {
     if (this.allCats.length === 0) {
       this.$store.commit("setCats");
-      this.loading = false;
     } else {
-      this.loading = false;
     }
   },
   components: { CatPicture },
